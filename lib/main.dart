8daba5a8d618c50de33fddeee35fc73893bd0854
feridungungor <62132762+feridungungor@users.noreply.github.com>
2020-03-13 11:39:17 +0300

    '13.03.2020'

---
+++ lib/main.dart
@@ -0,0 +1,172 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      body: Stack(fit: StackFit.expand, children: <Widget>[
+        ConstrainedBox(
+          constraints: const BoxConstraints.expand(),
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: new BoxDecoration(
+              shape: BoxShape.rectangle,
+              color: Colors.blue,
+            ),
+          ),
+        ), //ConstrainedBox
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: infoCard(context,"Hello Cınım"),
+            ),
+            SizedBox(height: 100,),
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: frostedIconButton(
+                  IconButton(
+                      icon: Icon(
+                        Icons.share,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {}),
+                ),
+              ),
+            )
+          ],
+        ),
+
+      ]), //Stack,
+      bottomNavigationBar: CurvedNavigationBar(
+        color: Colors.red,
+        buttonBackgroundColor: Colors.red,
+        backgroundColor: Colors.transparent,
+        height: 60,
+        animationDuration: Duration(milliseconds: 300),
+        animationCurve: Curves.ease,
+        index: 2,
+        items: <Widget>[
+          Icon(
+            Icons.verified_user,
+            size: 20,
+            color: Colors.black,
+          ),
+          Icon(
+            Icons.favorite,
+            size: 20,
+            color: Colors.black,
+          ),
+          Icon(
+            Icons.fullscreen_exit,
+            size: 20,
+            color: Colors.black,
+          ),
+          Icon(
+            Icons.add,
+            size: 20,
+            color: Colors.black,
+          ),
+          Icon(
+            Icons.list,
+            size: 20,
+            color: Colors.black,
+          ),
+        ],
+        onTap: (index) {
+          debugPrint("Current Index is $index");
+        },
+      ),
+    );
+  }
+}
+
+Widget frostedIconButton(Widget child) {
+  return ClipRRect(
+      borderRadius: BorderRadius.circular(50.0),
+      child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+          child: Container(
+              width: 50.0,
+              height: 50.0,
+              decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.3), shape: BoxShape.circle),
+              child: child)));
+}
+
+
+Widget infoCard(BuildContext context, String title) {
+  return ClipRRect(
+    borderRadius: BorderRadius.circular(15),
+    child: BackdropFilter(
+      filter: ImageFilter.blur(
+        sigmaX: 10.9,
+        sigmaY: 10.0,
+      ),
+      child: Container(
+        key: ValueKey<String>(title),
+        height: MediaQuery.of(context).size.height / 4,
+        width: MediaQuery.of(context).size.width / 1.2,
+        color: Colors.white.withOpacity(0.3),
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  title,
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Text(
+                "Feridun Güngör",
+                style: TextStyle(fontSize: 18.0, color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
